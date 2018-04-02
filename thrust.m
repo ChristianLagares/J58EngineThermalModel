d80166378052cacb9abdfeb1599e1b3ca9b12b0c
Christian J. Lagares Nieves <christian.lagares@upr.edu>
2018-04-02 18:04:47 -0400

    Added basic functionality to the thrust function
    Added the inputs and internal computation capability for thrust force
    computations. All operations are vectorized to allow for the evaluation
    of multiple flight conditions.

--- thrust.m
+++ thrust.m
@@ -2,7 +2,13 @@
 % 
 % INSERT DOC
 %% CODE
-function [output] = thrust()
-% Docstring
+function [output] = thrust(air_massflow, air2fuel, exhaust_velocity,...
+    flight_velocity, atmospheric_pressure, exhaust_pressure, exhaust_area)
+% thrust computes the thrust force for multiple flight conditions. Inputs
+% should be floating point vectors with one entry per flight condition.
 
+pthrust = (exhaust_pressure - atmospheric_pressure).*exhaust_area;
+vthrust = air_massflow.*((1+air2fuel).*exhaust_velocity + flight_velocity);
+
+output = pthrust + vthrust;
 end
