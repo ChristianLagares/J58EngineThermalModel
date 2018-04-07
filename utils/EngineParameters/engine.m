36aa226199fa2e76df3ceac9143c8629d26dc08c
Christian J. Lagares Nieves <christian.lagares@upr.edu>
2018-04-07 18:33:29 -0400

    Added basic engine specs
    Added thrust, sfc and thereby mass fuel rate

--- utils/EngineParameters/engine.m
+++ utils/EngineParameters/engine.m
@@ -3,10 +3,19 @@
 % mf
 
 %% CODE
-function [engine_specs] = engine(args)
+function [engine_specs] = engine()
 % DOCSTRING
-    WET_THRUST = 34000; % lbf
-    DRY_THRUST 
-    SFC = 1.9; % lb/lbf
-    engine_specs = struct('WET_THRUST', WET_THRUST);
+    WET_THRUST              = 34000;                       % lbf
+    DRY_THRUST              = 25000;                       % lbf
+    SFC                     = 1.9;                         % lb/lbf*hr
+    AFTERBURNER_FUEL_hr     = SFC * WET_THRUST;            % lb/hr
+    NO_AFTERBURNER_FUEL_hr  = SFC * DRY_THRUST;            % lb/hr
+    AFTERBURNER_FUEL_sec    = AFTERBURNER_FUEL_hr/3600;    %
+    NO_AFTERBURNER_FUEL_sec = NO_AFTERBURNER_FUEL_hr/3600;
+    engine_specs = struct('WET_THRUST', WET_THRUST,...
+                          'DRY_THRUST', DRY_THRUST,...
+                          'AFTERBURNER_FUEL_perHour', AFTERBURNER_FUEL_hr,...
+                          'NO_AFTERBURNER_FUEL_perHour', NO_AFTERBURNER_FUEL_hr,...
+                          'AFTERBURNER_FUEL_perSecond', AFTERBURNER_FUEL_sec,...
+                          'NO_AFTERBURNER_FUEL_perSecond', NO_AFTERBURNER_FUEL_sec);
 end
