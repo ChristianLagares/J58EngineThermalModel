356375c3bd352f3451ff94293e106a05910f4965
edwinapo95 <37989455+edwinapo95@users.noreply.github.com>
2018-04-15 15:03:46 -0400

    Added equation

--- PerfParameters/thermal_efficiency.m
+++ PerfParameters/thermal_efficiency.m
@@ -2,7 +2,11 @@
 %
 % INSERT DOC
 %% CODE
-function [output] = thermal_efficiency()
+function [output] = thermal_efficiency(air_massflow,air2fuel,exhaust_velocity,flight_velocity,LHV)
 % Docstring
 
+efther_num = (thrust.*flight_velocity) +0.5*air_massflow.*(1+air2fuel).*(exhaust_velocity-flight_velocity).^2
+efther_den = air_massflow.*air2fuel.*LHV
+
+output = efther_num./efther_den
 end
