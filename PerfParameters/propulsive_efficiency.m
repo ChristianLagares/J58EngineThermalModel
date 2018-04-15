eb95b6eeb8d4fbbe835178a9557c36c468f1001e
edwinapo95 <37989455+edwinapo95@users.noreply.github.com>
2018-04-15 15:02:40 -0400

    Added equation

--- PerfParameters/propulsive_efficiency.m
+++ PerfParameters/propulsive_efficiency.m
@@ -1,8 +1,12 @@
 %% Propulsive Efficiency
 %
-% INSERT DOC
+% 
 %% CODE
-function [output] = propulsive_efficiency()
+function [output] = propulsive_efficiency(air_massflow,air2fuel,exhaust_velocity,flight_velocity,thrust)
 % Docstring
 
+efprop_num = flight_velocity.*thrust;
+efprop_den = efprop_num + .5.*air_massflow.*(1+air2fuel).*(exhaust_velocity-flight_velocity).^2
+
+output= efprop_num./efprop_den
 end
